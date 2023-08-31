098e2c46dfc7ba431122b3d00616c5ec0fd140ab
Erick Etiene <333.erick@gmail.com>
2023-08-31 15:47:28 -0300

    Footer Component and Fixing Hero Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -28,8 +28,8 @@ const Hero = () => {
         <div className="hero__image-container">
             <div className="hero__image">
                 <Image src="/fakenews_side.png" alt="hero" fill className="object-contain"  />
-                <div className="hero__image-overlay"/>
             </div>
+            <div className="hero__image-overlay"/>
 
             
         </div>
