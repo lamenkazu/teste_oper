34a0fe2e589f2e4d06b4401db4a6858c408198a9
Erick Etiene <333.erick@gmail.com>
2023-08-31 15:30:24 -0300

    NavBar Component

--- components/CustomButton.tsx
+++ components/CustomButton.tsx
@@ -7,14 +7,15 @@ import { MouseEventHandler } from "react";
 interface CustomButtonProps{
     title: string;
     containerStyles?: string;
-    handleClick: MouseEventHandler<HTMLButtonElement>
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+    btnType?: "button" | "submit";
 }
 
-const CustomButton = ({title, containerStyles, handleClick}:CustomButtonProps) => {
+const CustomButton = ({title, containerStyles, handleClick, btnType = "button"}:CustomButtonProps) => {
   return (
     <button
     disabled={false}
-    type={"button"}
+    type={btnType}
     className={`custom-btn ${containerStyles}`}
     onClick={handleClick}
     >
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,26 @@
 import Link from "next/link";
 import Image from "next/image";
+import { CustomButton } from ".";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <header className="w-full absolute z-10">
+
+        <nav className="max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4">
+            <Link href='/' className="flex justify-center items-center">
+                <Image src="/fakenews.png" alt="FakeNewsLogo" 
+                        width={118} height={18}
+                        className="object-contain"/>
+            </Link>
+
+            <CustomButton 
+                title="SignIn" 
+                btnType="button"
+                containerStyles="text-primary-blue rounded-full bg-white min-w-[130px]"
+            />
+        </nav>
+
+    </header>
   )
 }
 
